669240df5cb927af14c137842461d29d73311d6a
zerg <anonzerg@proton.me>
2026-06-16 19:45:31 +0330

    append multiple frames.

--- xyz.py
+++ xyz.py
@@ -1,9 +1,12 @@
+from pathlib import Path
+
 class XYZ:
     def __init__(self, path):
-        self.path = path
+        self.path = Path(path)
+        self.path.write_text('')
         
     def write(self, number_of_atoms, comment_line, element, coords):
-        with open(self.path, 'w') as f:
+        with open(self.path, 'a') as f:
             f.write(f'{number_of_atoms}\n')
             f.write(f'{comment_line}\n')
             for x, y, z in coords:
